dfb2caaf0bb19079af54327bcae2a8f0ebcc1c1c
ayaup <ayaup@qq.com>
2017-12-30 20:35:40 +0000

    222

--- config/database.php
+++ config/database.php
@@ -65,7 +65,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
