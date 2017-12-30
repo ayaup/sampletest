663a24341cb27041ebeb6174529a72265f0ae538
ayaup <ayaup@qq.com>
2017-12-30 20:58:50 +0000

    444

--- config/database.php
+++ config/database.php
@@ -1,7 +1,30 @@
 <?php
-require dirname(dirname(FILE)).'/bootstrap/helpers.php';
+// require dirname(dirname(FILE)).'/bootstrap/helpers.php';
 
-$db_config = get_db_config();
+// $db_config = get_db_config();
+function get_db_config()
+
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    } else {
+        return $db_config = [
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+        ];
+    }
+}
 
 return [
 
@@ -67,7 +90,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
